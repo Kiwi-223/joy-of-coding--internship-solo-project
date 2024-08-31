72b712466b986f6eb68fa4cc245d8376ff68d0fa
Kiwi-223 <KiAndrewsKGA@gmail.com>
2024-08-31 17:59:23 -0600

    Updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="text-center">
       <div className="text-4xl m-4">Welcome to ToDo Task Manager</div>
-      <div>Login to view your tasks.</div>
+      <div>
+        Click
+        <Link href={"/tasks"} className="text-green-700">
+          {" "}
+          here{" "}
+        </Link>
+        to view all tasks
+      </div>
+      <div>
+        Click
+        <Link href={"/tasks/new"} className="text-green-700">
+          {" "}
+          here{" "}
+        </Link>
+        to create a new task
+      </div>
     </div>
   );
 }
